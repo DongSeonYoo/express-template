f60f3d9e0887b8d3efa20c4825ef68953b9144ea
ehdtjs0612 <inko51366@naver.com>
2024-07-14 03:29:12 +0900

    refactor: use logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
 import { env } from './configs/env.config';
-import { setGlobalMiddleware } from './utils/loaders/global.loader';
-import { setRouteMiddleware } from './utils/loaders/router.loader';
-import { setExceptionFilter } from './utils/loaders/error-middleware.loader';
+import { setGlobalMiddleware } from './utils/loaders/global-middleware.loader';
+import { setRouteMiddleware } from './utils/loaders/route-middleware.loader';
+import { setExceptionFilter } from './utils/loaders/exception-filter.loader';
+import { logger } from './configs/logger.config';
 
 async function startServer() {
   const app = express();
@@ -14,7 +15,7 @@ async function startServer() {
   setExceptionFilter(app);
 
   app.listen(HTTP_PORT, '0.0.0.0', () => {
-    console.log(`server on ${HTTP_PORT}`);
+    logger.log('info', `server on ${HTTP_PORT}`);
   });
 }
 
